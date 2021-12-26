abc14f9b47a7ce6557be2af946bcf1caa43273de
Afonso Neto <afonsoneto121@gmail.com>
2021-12-26 17:18:40 -0300

    Initial commit
    * Docker configuration

---
+++ transfer/src/main/java/com/dio/tqi/transfer/TransferApplication.java
@@ -0,0 +1,13 @@
+package com.dio.tqi.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferApplication.class, args);
+	}
+
+}
